d53b850bc1f74371bfd760ae78b04e3cd5a50122
Martin Feckie <martin.feckie@3plearning.com>
2023-03-27 20:19:52 +0800

    Initialize project

---
+++ lib/ammoniex.ex
@@ -0,0 +1,18 @@
+defmodule Ammoniex do
+  @moduledoc """
+  Documentation for `Ammoniex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ammoniex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ammoniex_test.exs
@@ -0,0 +1,8 @@
+defmodule AmmoniexTest do
+  use ExUnit.Case
+  doctest Ammoniex
+
+  test "greets the world" do
+    assert Ammoniex.hello() == :world
+  end
+end
