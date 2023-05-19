b5029e29965a59b774c1d21e0ae311bd11bdcae2
Martin Feckie <martin.feckie@3plearning.com>
2023-05-19 15:29:37 +0800

    Bump deps

--- mix.exs
+++ mix.exs
@@ -41,8 +41,8 @@ defmodule Ammoniex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "0.27.0"},
-      {:ex_doc, "~> 0.29.2"}
+      {:rustler, "0.28.0"},
+      {:ex_doc, "~> 0.29.4"}
     ]
   end
 end
